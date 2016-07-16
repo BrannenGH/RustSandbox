27fab4981e92f76eb968278994d1dc2b9ecf4ed7
Brannen Hall <Brannen@HurtadoHall.me>
2016-07-15 21:32:19 -0500

    Got rid of the recursive function

--- splurthian_chemistry_101/src/main.rs
+++ splurthian_chemistry_101/src/main.rs
@@ -8,19 +8,19 @@ fn main() {
     io::stdin.readline(&mut element).expect("Failed to read full element name! Exiting");
     io::stdin.readline(&mut symbol).expect("Failed to read symbol! Exiting");
 
-    if (check(&element, &symbol)) == True{
+    if (check(&element, &symbol)) == true{
         println!("Symbol Works");
     } else{
         println!("Does not work, try again.")
     }
 }
 
-fn check(element: &str, symbol: &str) -> []{
-    if rec_check(element, symbol.chars().nth(1)) && rec_check()== True{
-        if rec_check
+fn check(element: &str, symbol: &str) -> bool{
+    for charater in element.chars(){
+        if character == symbol.chars().nth(0){
+            char
+            //Find a way to copy contents to a new string, cut that string, and then iterate over that.
+        }else{return false;}
     }
-}
-
-fn rec_check(element: &str letter: &str) -> bool{
-
+    }
 }

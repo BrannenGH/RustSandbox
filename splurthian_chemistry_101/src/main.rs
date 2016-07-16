19f8562213e11bee0f9d2cac07ec998510f76356
Brannen Hall <Brannen@HurtadoHall.me>
2016-07-16 15:50:20 -0500

    Added some new str and slice functions, may not keep slice.

--- splurthian_chemistry_101/src/main.rs
+++ splurthian_chemistry_101/src/main.rs
@@ -1,26 +1,34 @@
 use std::io;
 use std::str;
+use std::char;
+use std::slice;
 
 fn main() {
     let mut element = String::new();
     let mut symbol = String::new();
 
-    io::stdin.readline(&mut element).expect("Failed to read full element name! Exiting");
-    io::stdin.readline(&mut symbol).expect("Failed to read symbol! Exiting");
+    io::stdin().read_line(&mut element).expect("Failed to read full element name! Exiting");
+    io::stdin().read_line(&mut symbol).expect("Failed to read symbol! Exiting");
 
-    if (check(&element, &symbol)) == true{
+    if assert_eq!(check(&element, &symbol),true){
         println!("Symbol Works");
     } else{
-        println!("Does not work, try again.")
+        println!("Does not work, try again.");
     }
 }
 
 fn check(element: &str, symbol: &str) -> bool{
-    for charater in element.chars(){
-        if character == symbol.chars().nth(0){
-            char
-            //Find a way to copy contents to a new string, cut that string, and then iterate over that.
-        }else{return false;}
-    }
+    for character in element.char_indicies(){
+        if assert_eq!(character.1.to_lowercase(), symbol.chars().nth(0).to_lowercase()){
+            let splitTup = element.split_at(character.0 * 4); //assumes char is four bytes and is in latin based UTF-8, won't work with non-UTF8
+            for newChar in splitTup.1.chars(){
+                if assert_eq!(newChar.to_lowercase(), symbol.chars().nth(1).to_lowercase()){
+                    return true;
+                }
+            }
+        }
+        else{
+            return false;
+        }
     }
 }

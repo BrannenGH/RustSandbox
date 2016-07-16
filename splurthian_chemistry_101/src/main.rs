053a6f5f72781851bd48eb13829e6b71f6f28445
Brannen Hall <Brannen@HurtadoHall.me>
2016-07-15 21:11:51 -0500

    Added recursive function

--- splurthian_chemistry_101/src/main.rs
+++ splurthian_chemistry_101/src/main.rs
@@ -1,10 +1,26 @@
 use std::io;
+use std::str;
 
 fn main() {
-    let mut input = String::new();
-    io::stdin.readline(&mut input).expect("Failed to read line! Exiting");
+    let mut element = String::new();
+    let mut symbol = String::new();
+
+    io::stdin.readline(&mut element).expect("Failed to read full element name! Exiting");
+    io::stdin.readline(&mut symbol).expect("Failed to read symbol! Exiting");
+
+    if (check(&element, &symbol)) == True{
+        println!("Symbol Works");
+    } else{
+        println!("Does not work, try again.")
+    }
 }
 
-fn check(element: &str symbol: &str) -> result<bool, std{
+fn check(element: &str, symbol: &str) -> []{
+    if rec_check(element, symbol.chars().nth(1)) && rec_check()== True{
+        if rec_check
+    }
+}
+
+fn rec_check(element: &str letter: &str) -> bool{
 
 }
